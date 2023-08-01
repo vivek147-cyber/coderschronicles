494b1140ca8f96a3f0313e81ac2a4f8a654d4032
vivek singh <65790028+vivek147-cyber@users.noreply.github.com>
2023-08-01 19:31:18 +0530

    final

--- coderschronicles/settings.py
+++ coderschronicles/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# STATIC_ROOT ='/home/uhmqxqjd/public_html/static'
-# MEDIA_ROOT ='/home/uhmqxqjd/public_html/media'
+STATIC_ROOT ='/home/uhmqxqjd/public_html/static'
+MEDIA_ROOT ='/home/uhmqxqjd/public_html/media'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
